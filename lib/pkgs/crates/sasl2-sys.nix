9666785e279fb7e611fefdf90b3df6940aa75d7e
nihirash <a.s.sharikhin@ringcentral.com>
2025-12-15 13:36:09 +0100

    sasl2 fix

--- lib/pkgs/crates/sasl2-sys.nix
+++ lib/pkgs/crates/sasl2-sys.nix
@@ -3,13 +3,27 @@
 , cyrus_sasl
 }:
 
-mkEnvHook {
-  name = "cargo-sasl2-sys";
+let
+  targetPkgs = if pkgs ? pkgsTargetTarget then pkgs.pkgsTargetTarget else pkgs;
+  isStatic = pkgs.stdenv.hostPlatform.isStatic or false;
 
-  propagatedBuildInputs = [
-    pkgs.pkgsBuildHost.pkg-config
-  ];
-  depsTargetTargetPropagated = [
-    cyrus_sasl
-  ];
+  # Override cyrus_sasl to disable PAM for static builds (linux-pam is not available for static targets)
+  cyrus_sasl_base = targetPkgs.cyrus_sasl or cyrus_sasl;
+  cyrus_sasl_overridden = if cyrus_sasl_base ? override then
+    cyrus_sasl_base.override ({
+      enableLdap = false;
+    } // (if isStatic then { pam = null; } else {}))
+  else
+    cyrus_sasl_base;
+  cyrus_sasl_no_db_target = cyrus_sasl_overridden.overrideAttrs (old: {
+    configureFlags = (old.configureFlags or []) ++ [
+      "--disable-sasldb"
+      "--with-dblib=none"
+    ] ++ (if isStatic then [ "--disable-login" "--disable-gssapi" ] else []);
+  });
+in
+mkEnvHook {
+  name = "sasl2-sys";
+  propagatedBuildInputs = [ pkgs.pkgsBuildHost.pkg-config ];
+  depsTargetTargetPropagated = [ cyrus_sasl_no_db_target ];
 }
